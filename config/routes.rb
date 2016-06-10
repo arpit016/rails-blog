234e8aaa8ba4d14969aeeedbaf514e6c12816964
Arpit Agarwal <agarwalarpit22@gmail.com>
2016-06-10 18:45:44 +0000

    Changed routes from default homepage to custom homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   
-  get 'pages/home' => 'pages#home'
+  root 'pages#home'
   
-  get 'pages/signup' => 'pages#signup'
+  get 'signup' => 'pages#signup'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
