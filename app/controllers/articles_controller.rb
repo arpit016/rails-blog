2bd4e66fc7a910610bd408c06f4e4ce395883d4d
Arpit Agarwal <agarwalarpit22@gmail.com>
2016-06-11 15:44:08 +0000

    Added Editing articles functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,10 @@
 class ArticlesController < ApplicationController
   
+  def index
+    
+  end
+  
+  
   def new
     @article = Article.new
   end
@@ -7,6 +12,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
+      puts "IDDDDDDDDDDDDDDDDD "+ @article.id.to_s
       flash[:notice] = "Article was successfully created"
       redirect_to article_path(@article)
     else
@@ -15,7 +21,22 @@ class ArticlesController < ApplicationController
   end
   
   def show
+    puts params
     @article = Article.find(params[:id])
+  end
+  
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was updated successfully"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
   end
   
   private
