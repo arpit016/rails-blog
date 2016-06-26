5ecabd3812a90d81f612f6036a2c074697b7e48b
Arpit Agarwal <agarwalarpit22@gmail.com>
2016-06-26 19:34:33 +0000

    Added case sensitivity in unique validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ActiveRecord::Base
-  validates :username, presence: true, length: {minimum:3, maximum: 30}
-  validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+  validates :username, presence: true, 
+            length: {minimum:3, maximum: 30}, 
+            uniqueness: { case_sensitive: false }
+
+  validates :email, presence: true, 
+            format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }, 
+            uniqueness: { case_sensitive: false },
+            length: { maximum: 105 }
 end
