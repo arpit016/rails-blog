c3c589bde4f601171ebb432b3d9bb0c3d196c8c8
Arpit Agarwal <agarwalarpit22@gmail.com>
2016-07-02 04:50:29 +0000

    SignUp users from front end and backend

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   
   root 'pages#home'
   
-  get 'signup' => 'pages#signup'
+  get 'signup', to: 'users#new'
   
   resources :articles
+  
+  resources :users, except: [:new]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
